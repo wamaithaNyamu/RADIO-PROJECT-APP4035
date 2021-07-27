0aaa57024376729817c0e7fddfcc7f3a6a752519
Victor Kamoto <victorkamoto@gmail.com>
2021-07-27 17:00:53 +0300

    server setup

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const cors = require("cors");
+const { json } = require("express");
+
+// import route files
+
+// App config
+dotenv.config({
+  path: "config/.env",
+});
+// Initialize app
+const app = express();
+app.use(cors());
+app.use(json());
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Connect to Database
+
+// Mount routers
+
+const PORT = process.env.PORT;
+
+// Listen on HTTP
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.red
+  );
+});
